4d6707839d59e44ef11f4003a758198a652df25f
richdrob <richdrob@umich.edu>
2026-01-13 18:41:08 -0500

    Quartz sync: Jan 13, 2026, 6:41 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -88,8 +88,7 @@ const config: QuartzConfig = {
       Plugin.Static(),
       Plugin.Favicon(),
       Plugin.NotFoundPage(),
-      // Comment out CustomOgImages to speed up build time
-      Plugin.CustomOgImages({colorScheme: "darkMode",}),
+           Plugin.CustomOgImages({colorScheme: "darkMode",}),
     ],
   },
 }
