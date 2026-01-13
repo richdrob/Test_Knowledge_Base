d989dbf8080343e5969edcfb68f5eb896f590047
richdrob <richdrob@umich.edu>
2026-01-13 18:43:24 -0500

    Quartz sync: Jan 13, 2026, 6:43 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://richdrob.github.io/Test_Knowledge_Base/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
